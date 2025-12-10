52880b84b6ab479476345b63db76a5febc214d6d
Guillermo Hernandez Ruiz de Esparza <guillermo.hre@gmail.com>
2025-12-10 11:10:21 -0600

    Add DML script for inserting records into entidad table

---
+++ DML/operacion/04-entidades.sql
@@ -0,0 +1,74 @@
+--@Autor: 
+--@Fecha creación: 
+--@Descripción:
+
+whenever sqlerror exit rollback
+
+Prompt - Iniciando insercion de registros en la tabla entidad
+
+INSERT INTO entidad (clave, nombre) 
+    VALUES ('00AGS', 'Aguascalientes');
+INSERT INTO entidad (clave, nombre) 
+    VALUES ('000BC', 'Baja California');
+INSERT INTO entidad (clave, nombre) 
+    VALUES ('00BCS', 'Baja California Sur');
+INSERT INTO entidad (clave, nombre) 
+    VALUES ('00CAM', 'Campeche');
+INSERT INTO entidad (clave, nombre) 
+    VALUES ('00COA', 'Coahuila');
+INSERT INTO entidad (clave, nombre) 
+    VALUES ('00COL', 'Colima');
+INSERT INTO entidad (clave, nombre) 
+    VALUES ('0CHIS', 'Chiapas');
+INSERT INTO entidad (clave, nombre) 
+    VALUES ('00CHH', 'Chihuahua');
+INSERT INTO entidad (clave, nombre) 
+    VALUES ('00DUR', 'Durango');
+INSERT INTO entidad (clave, nombre) 
+    VALUES ('00GTO', 'Guanajuato');
+INSERT INTO entidad (clave, nombre) 
+    VALUES ('00GRO', 'Guerrero');
+INSERT INTO entidad (clave, nombre) 
+    VALUES ('00HGO', 'Hidalgo');
+INSERT INTO entidad (clave, nombre) 
+    VALUES ('00JAL', 'Jalisco');
+INSERT INTO entidad (clave, nombre) 
+    VALUES ('00MEX', 'México');
+INSERT INTO entidad (clave, nombre) 
+    VALUES ('0MICH', 'Michoacán');
+INSERT INTO entidad (clave, nombre) 
+    VALUES ('00MOR', 'Morelos');
+INSERT INTO entidad (clave, nombre) 
+    VALUES ('00NAY', 'Nayarit');
+INSERT INTO entidad (clave, nombre) 
+    VALUES ('000NL', 'Nuevo León');
+INSERT INTO entidad (clave, nombre) 
+    VALUES ('00OAX', 'Oaxaca');
+INSERT INTO entidad (clave, nombre) 
+    VALUES ('00PUE', 'Puebla');
+INSERT INTO entidad (clave, nombre) 
+    VALUES ('00QRO', 'Querétaro');
+INSERT INTO entidad (clave, nombre) 
+    VALUES ('000QR', 'Quintana Roo');
+INSERT INTO entidad (clave, nombre) 
+    VALUES ('00SLP', 'San Luis Potosí');
+INSERT INTO entidad (clave, nombre) 
+    VALUES ('00SIN', 'Sinaloa');
+INSERT INTO entidad (clave, nombre) 
+    VALUES ('00SON', 'Sonora');
+INSERT INTO entidad (clave, nombre) 
+    VALUES ('00TAB', 'Tabasco');
+INSERT INTO entidad (clave, nombre) 
+    VALUES ('00TAM', 'Tamaulipas');
+INSERT INTO entidad (clave, nombre) 
+    VALUES ('0TLAX', 'Tlaxcala');
+INSERT INTO entidad (clave, nombre) 
+    VALUES ('00VER', 'Veracruz');
+INSERT INTO entidad (clave, nombre) 
+    VALUES ('00YUC', 'Yucatán');
+INSERT INTO entidad (clave, nombre) 
+    VALUES ('00ZAC', 'Zacatecas');
+INSERT INTO entidad (clave, nombre) 
+    VALUES ('0CDMX', 'Ciudad de México');
+
+Prompt > Insercion de registros en la tabla entidad completada
