6424e25c8505ae7e85398c243e3bd327b2f151d6
Guillermo Hernandez Ruiz de Esparza <guillermo.hre@gmail.com>
2025-12-10 10:25:29 -0600

    Add initial DML script for record insertion and trigger creation

---
+++ DML/01-main-DML.sql
@@ -0,0 +1,16 @@
+--@Autor: 
+--@Fecha creación: 
+--@Descripción:
+
+whenever sqlerror exit rollback
+
+Prompt ========================================================
+Prompt Iniciando insercion de registros creacion de procedimientos y triggers
+Prompt ========================================================
+
+@DML/cliente/11-main-DML-clientes.sql
+@DML/operacion/21-main-DML-operaciones.sql
+
+Prompt ========================================================
+Prompt Insercion de registros, creacion de procedimientos y triggers completada
+Prompt ========================================================
