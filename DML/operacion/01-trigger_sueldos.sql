7625da72fb0bfe42d82b749cd16c4fb27da1361c
Guillermo Hernandez Ruiz de Esparza <guillermo.hre@gmail.com>
2025-12-10 10:22:43 -0600

    Add trigger for updating salary in puesto table with error handling

---
+++ DML/operacion/01-trigger_sueldos.sql
@@ -0,0 +1,35 @@
+--@Autor: 
+--@Fecha creación: 
+--@Descripción:
+
+whenever sqlerror exit rollback
+
+Prompt - Iniciando creacion de trigger para de sueldo en tabla puesto
+
+CREATE OR REPLACE TRIGGER TRG_SUELDO_PUESTO
+AFTER UPDATE OF sueldo ON puesto
+FOR EACH ROW
+DECLARE
+    v_fecha_fin historico_sueldo.fecha_fin%TYPE;
+BEGIN
+    DBMS_OUTPUT.PUT_LINE('- Iniciando insercion de registro en la tabla historico_sueldo');
+    v_fecha_fin := :NEW.inicio_sueldo - 1;
+    
+    BEGIN
+        INSERT INTO historico_sueldo(sueldo_actualizado, fecha_inicio, fecha_fin, puesto_id)
+        VALUES (:OLD.sueldo, :OLD.inicio_sueldo, v_fecha_fin, :NEW.puesto_id);
+    EXCEPTION
+        WHEN OTHERS THEN
+            DBMS_OUTPUT.PUT_LINE('* Error al insertar historico_sueldo: ' || SQLERRM);
+    END;
+
+    COMMIT;
+
+    DBMS_OUTPUT.PUT_LINE('> Insercion de registro en la tabla historico_sueldo completada');
+EXCEPTION
+    WHEN OTHERS THEN
+        DBMS_OUTPUT.PUT_LINE('* Error en el trigger TRG_SUELDO_PUESTO: ' || SQLERRM);
+END TRG_SUELDO_PUESTO;
+/
+
+Prompt > Creacion de trigger para de sueldo en tabla puesto completada
