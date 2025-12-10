fed4d3b071859b67f8cfbdbacd2895eca3982334
Guillermo Hernandez Ruiz de Esparza <guillermo.hre@gmail.com>
2025-12-10 14:09:24 -0600

    Create 09-modelos.sql for inserting records into modelo table

---
+++ DML/operacion/09-modelos.sql
@@ -0,0 +1,50 @@
+--@Autor: 
+--@Fecha creación: 
+--@Descripción:
+
+whenever sqlerror exit rollback
+
+Prompt - Iniciando insercion de registros en la tabla modelo
+
+INSERT INTO modelo (marca, modelo)
+    VALUES ('Freightliner', 'M2 106');
+INSERT INTO modelo (marca, modelo)
+    VALUES ('Freightliner', 'SD 108');
+INSERT INTO modelo (marca, modelo)
+    VALUES ('Freightliner', 'Coronado SD');
+INSERT INTO modelo (marca, modelo)
+    VALUES ('Kenworth', 'T880');
+INSERT INTO modelo (marca, modelo)
+    VALUES ('Kenworth', 'T440');
+INSERT INTO modelo (marca, modelo)
+    VALUES ('Kenworth', 'W990');
+INSERT INTO modelo (marca, modelo)
+    VALUES ('International', 'WorkStar');
+INSERT INTO modelo (marca, modelo)
+    VALUES ('International', 'HX Series');
+INSERT INTO modelo (marca, modelo)
+    VALUES ('International', 'DuraStar');
+INSERT INTO modelo (marca, modelo)
+    VALUES ('Peterbilt', 'Model 567');
+INSERT INTO modelo (marca, modelo)
+    VALUES ('Peterbilt', 'Model 367');
+INSERT INTO modelo (marca, modelo)
+    VALUES ('Peterbilt', 'Model 348');
+INSERT INTO modelo (marca, modelo)
+    VALUES ('Mack', 'Granite');
+INSERT INTO modelo (marca, modelo)
+    VALUES ('Mack', 'TerraPro');
+INSERT INTO modelo (marca, modelo)
+    VALUES ('Mack', 'Anthem');
+INSERT INTO modelo (marca, modelo)
+    VALUES ('Volvo', 'VHD');
+INSERT INTO modelo (marca, modelo)
+    VALUES ('Volvo', 'VNX');
+INSERT INTO modelo (marca, modelo)
+    VALUES ('Mercedes-Benz', 'Actros');
+INSERT INTO modelo (marca, modelo)
+    VALUES ('Scania', 'P-Series');
+INSERT INTO modelo (marca, modelo)
+    VALUES ('Hino', 'Serie 500');
+
+Prompt > Insercion de registros en la tabla modelo completada
