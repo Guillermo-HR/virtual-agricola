afaed0bb646397b962a9f041e0a5ef9ff0a933c3
Guillermo Hernandez Ruiz de Esparza <guillermo.hre@gmail.com>
2025-12-12 10:14:32 -0600

    Actualiza referencia a script de operaciones en DML principal

--- DML/01-main-DML.sql
+++ DML/01-main-DML.sql
@@ -9,7 +9,7 @@ Prompt Iniciando insercion de registros creacion de procedimientos y triggers
 Prompt ========================================================
 
 @DML/cliente/11-main-DML-clientes.sql
-@DML/operacion/21-main-DML-operaciones.sql
+@DML/operacion/22-main-DML-operaciones.sql
 
 Prompt ========================================================
 Prompt Insercion de registros, creacion de procedimientos y triggers completada
